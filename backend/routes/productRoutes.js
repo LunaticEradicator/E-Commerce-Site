72e09c53ddde622035231ba17d9531b1a8442eb6
YeduKrishnan A <kuttuzz17@gmail.com>
2023-08-01 15:44:46 +0530

    Fetch Product from database

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,14 @@
-import products from "../data/products.js";
+// import products from "../data/products.js";
 import express from "express";
 import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
 
 const router = express.Router();
 
 router.get(
   "/",
   asyncHandler(async (req, res) => {
+    const products = await Product.find({});
     res.json(products);
   })
 );
@@ -14,7 +16,7 @@ router.get(
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
-    const selectedProduct = products.find((p) => p._id === req.params.id);
+    const selectedProduct = await Product.findById(req.params.id);
     if (selectedProduct) {
       res.json(selectedProduct);
     } else {
